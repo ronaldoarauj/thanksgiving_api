dc70f46006e3b6b806e7d0dd764869d3ab8581a2
ronaldo araujo <ronaldoaraujo@jet.com.br>
2024-01-26 19:19:41 -0300

    inserido a home

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Página Inicial!</h1>
+      <p>Esta é a sua página inicial Next.js com TypeScript.</p>
+    </div>
+  );
+};
+
+export default Home;
