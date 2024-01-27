f871e3df955b0152f4280ab7e028bf759cd68340
ronaldo araujo <ronaldoaraujo@jet.com.br>
2024-01-26 21:44:56 -0300

    teste Teste

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         if (method === 'GET') {
             const response = await userController.findByID(query.id as string);
-            return res.status(201).json({ message: 'ok', data: response })
+            return res.status(200).json({ message: 'ok', data: response })
         }
         const response = await userController.findAllUsers()
         return res.status(200).json({ message: 'ok', data: response })
